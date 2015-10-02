ca53cabbd15cd40faa55650e0f93993bb0850aa0
mhmuftee <mhmuftee@gmail.com>
2015-10-03 00:38:11 +0600

    High-Precision Number

---
+++ Java BigInteger Class/Bonus Features: Others/11821.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Main{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int kase = 0;
+    BigDecimal sum = BigDecimal.ZERO;
+    kase = in.nextInt();
+    while(kase>0){
+      BigDecimal number = in.nextBigDecimal();
+      if(number.equals(BigDecimal.ZERO)){
+        kase--;
+        String s = sum.toPlainString();
+        int i,len = s.length();
+        for(i = len-1; s.charAt(i)=='0';i--);
+        if(s.charAt(i)=='.')i--;
+        System.out.println(s.substring(0,i+1));
+        sum = BigDecimal.ZERO;
+      }
+      else {
+        sum = sum.add(number);
+      }
+    }  
+  }
+}
