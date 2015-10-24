580465d9b32ad9582a84ce4ae5572f074042377f
Muftee <mhmuftee@gmail.com>
2015-10-24 09:52:44 +0600

    Numerical surprises

---
+++ Java BigInteger Class/Basic Features/1226.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    char input[2005];
+    long div,remainder;
+
+    int cas,i;
+
+    scanf("%d",&cas);
+    while(cas--)
+    {
+        scanf("%ld",&div);
+        scanf("%s",input);
+        remainder=0;
+        for(i=0; input[i]!=0; i++)
+        {
+            remainder=remainder*10+(input[i]-'0');
+            remainder%=div;
+        }
+        printf("%ld\n",remainder);
+    }
+    return 0;
+}
