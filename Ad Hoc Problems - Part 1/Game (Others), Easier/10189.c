d95a144829fb2c088ec3d5da26d495aac2597670
Muftee <mhmuftee@gmail.com>
2015-10-29 21:27:44 +0600

    Minesweeper

--- Ad Hoc Problems - Part 1/Game (Others), Easier/10189.c
+++ Ad Hoc Problems - Part 1/Game (Others), Easier/10189.c
@@ -1,22 +1,26 @@
 #include<stdio.h>
 #include<string.h>
-int main() {
+int main()
+{
     char ch,c[105][105];
     int i,j,k,n,m,p;
     p=1;
 
     int h=1;
-    while(scanf("%d %d",&m,&n)!=EOF) {
+    while(scanf("%d %d",&m,&n)!=EOF)
+    {
 
         if(m==0 && n==0)break;
-        for(i=0; i<=m+1; i++) {
+        for(i=0; i<=m+1; i++)
+        {
             for(j=0; j<=n+1; j++)
                 c[i][j]='1';
         }
 
         scanf("%c",&ch);
 
-        for(i=1; i<=m; i++) {
+        for(i=1; i<=m; i++)
+        {
             for(j=1; j<=n; j++)
                 scanf("%c",&c[i][j]);
             scanf("%c",&ch);
@@ -33,35 +37,44 @@ int main() {
             for(j=1; j<=n; j++)
                 if(c[i][j]=='0')
                 {
-                    if(c[i][j+1]=='*') {
+                    if(c[i][j+1]=='*')
+                    {
                         c[i][j]++;
                     }
-                    if (c[i][j-1]=='*') {
+                    if (c[i][j-1]=='*')
+                    {
                         c[i][j]++;
                     }
-                    if(c[i-1][j]=='*') {
+                    if(c[i-1][j]=='*')
+                    {
                         c[i][j]++;
                     }
-                    if(c[i+1][j]=='*') {
+                    if(c[i+1][j]=='*')
+                    {
                         c[i][j]++;
                     }
-                    if(c[i-1][j+1]=='*') {
+                    if(c[i-1][j+1]=='*')
+                    {
                         c[i][j]++;
                     }
-                    if(c[i-1][j-1]=='*') {
+                    if(c[i-1][j-1]=='*')
+                    {
                         c[i][j]++;
                     }
-                    if(c[i+1][j-1]=='*') {
+                    if(c[i+1][j-1]=='*')
+                    {
                         c[i][j]++;
                     }
-                    if(c[i+1][j+1]=='*') {
+                    if(c[i+1][j+1]=='*')
+                    {
                         c[i][j]++;
                     }
 
                 }
         if(p!=1)printf("\n");
         printf("Field #%d:\n",p);
-        for(i=1; i<=m; i++) {
+        for(i=1; i<=m; i++)
+        {
             for(j=1; j<=n; j++)
                 printf("%c",c[i][j]);
             printf("\n");
