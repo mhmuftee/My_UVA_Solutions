48e74ffb2f7ca5962a399e7a17941d3daf778e59
mhmuftee <mhmuftee@gmail.com>
2015-11-10 00:38:15 +0600

    The Decoder

---
+++ Ad Hoc String Processing Problems - Part 1/Cipher,Encode,Encrypt,Decode,Decrypt, Easier/458.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    while(scanf("%c", &c)==1 )
+    {
+        if(c == '\n')
+            printf("\n");
+        else
+            printf("%c",ch-7);
+    }
+    return 0 ;
+}
